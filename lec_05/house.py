55917873dd6c1d8d7397aab886637469a3df390d
pseudowolf <ed.suzi@gmal.com>
2021-02-08 13:08:06 +0200

    Adding walls, roof and foundation functions prototypes. Have to make function workign

--- lec_05/house.py
+++ lec_05/house.py
@@ -1,3 +1,39 @@
+def main():
+    x, y = 300, 200
+    width, height = 200, 300
+
+    draw_house(x, y, width, height)
+
+
+def draw_house_foudation(x, y, width, height):
+    """
+
+    :param x:
+    :param y:
+    :param width:
+    :param height:
+    :return:
+    """
+    print('Типа рисую фундамент...', x, y, width, height)
+    pass
+
+def draw_house_walls(x, y, width, height):
+    """
+
+    :param x:
+    :param y:
+    :param width:
+    :param height:
+    :return:
+    """
+    print('Типа рисую стены...', x, y, width, height)
+    pass
+
+def draw_house_roof(x, y, width, height):
+    """"""
+    print('Типа рисую крышу...', x, y, width, height)
+    pass
+
 
 def draw_house(x, y, width, height):
     """
@@ -10,9 +46,13 @@ def draw_house(x, y, width, height):
     :return: None
     """
     print('Типа рисую дом...', x, y, width, height)
-    pass
+    foundation_height = 0.05 * height
+    walls_height = 0.5 * width
+    walls_width = 0.9 * width
+    roof_height = height - foundation_height - walls_height
+    draw_house_foudation(x, y, width, foundation_height)
+    draw_house_walls(x, y - foundation_height, walls_width, walls_height)
+    draw_house_roof(x, y - foundation_height - walls_height, width, roof_height)
 
-x, y = 100, 100
-width, height = 200, 300
 
-draw_house(x, y, width, height)
+main()
