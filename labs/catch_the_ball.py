b98d987cfe689a2814c978b7bef84a6267ae48a5
pseudowolf <ed.suzi@gmal.com>
2021-02-09 11:44:12 +0200

    SCORE variable get on display and it changes when player catches the ball.

--- labs/catch_the_ball.py
+++ labs/catch_the_ball.py
@@ -9,6 +9,7 @@ FPS = 2
 screen = pygame.display.set_mode((400, 400))
 pygame.display.set_caption('Catch the ball!')
 
+# Color variables
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
@@ -16,9 +17,27 @@ GREEN = (0, 255, 0)
 MAGENTA = (255, 0, 255)
 CYAN = (0, 255, 255)
 BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
 COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
 
+# Variable to store player's score
 SCORE: int = 0
+
+# create a font object.
+# 1st parameter is the font file
+# which is present in pygame.
+# 2nd parameter is size of the font
+font = pygame.font.Font("freesansbold.ttf", 20)
+# create a text suface object,
+# on which text is drawn on it.
+text = font.render('SCORE: '+str(SCORE), True, BLUE, WHITE)
+# create a rectangular object for the
+# text surface object
+textRect = text.get_rect()
+# Set position of the textRect
+textRect.topleft = (5, 5)
+
+
 
 
 def new_ball():
@@ -65,8 +84,14 @@ def is_click_inside_ball(x1, x2, y1, y2, r) -> bool:
 
 
 def update_score():
+    """
+    Update SCORE variable
+    :return: None
+    """
+    global text
     global SCORE
     SCORE += 1
+    text = font.render('SCORE: '+str(SCORE), True, BLUE, WHITE)
 
 
 pygame.display.update()
@@ -76,15 +101,21 @@ finished = False
 # Game loop begins
 while not finished:
     clock.tick(FPS)
+    # copying the text surface object
+    # to the display surface object
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
             click(event)
 
+
     new_ball()
+    screen.blit(text, textRect)
     pygame.display.update()
-    screen.fill(BLACK)
+    screen.fill(WHITE)
+
+
 
 pygame.quit()
 
